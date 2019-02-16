9ff18d7232473df0bfe5ee12d0f844056547f7be
Ben Dominguez <bendominguez011@gmail.com>
2019-02-16 01:09:05 -0500

    completed registrations

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField,\
-SubmitField, SelectField
+SubmitField, SelectField, DateTimeField
 from wtforms.validators import ValidationError, DataRequired, DataRequired, Email,\
 EqualTo
-from app.models import User
+from app.models import User, Startup
 
 company_types = ['Limited Liability Corporation', 'Limited Partnership', 'S-Corp', 'Corporation (Inc.)', 'Sole Proprietorship', 'Partnership']
 
@@ -12,6 +12,12 @@ states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA",
           "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
           "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
           "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+
+jobs = ['iOS Developement', 'Full-Stack Web Dev', 'Front-End Web Dev', 'Backend Web Dev']
+
+states_tup = tuple(zip(states, states))
+company_types_tup = tuple(zip(company_types, company_types))
+jobs_tup = tuple(zip(jobs, jobs))
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -46,3 +52,23 @@ class FirstRegistrationForm(FlaskForm):
 class SecondRegistrationForm(FlaskForm):
     role = SelectField('Are you an employer or freelancer?', choices=[('Employer', 'Employer'), ('Freelancer', 'Freelancer')])
     submit = SubmitField('Submit')
+
+class EmployerRegistrationForm(FlaskForm):
+    company_name = StringField("Company Name", validators=[DataRequired()])
+    state_of_incorporation = SelectField("What's your state of incorporation?", choices=states_tup, validators=[DataRequired()])
+    company_type = SelectField("What type of company are you?", choices=company_types_tup, validators=[DataRequired()])
+    #Im taking out date of incorporation for now
+    # date_of_incorporation = DateTimeField("Date of incorporation?", validators=[DataRequired()]) #matches with founded date, need to update models
+    taxID = StringField("Tax ID (if US)", validators=[DataRequired()])
+    submit = SubmitField("Finish registration for now!")
+
+    def validate_company_name(self, company_name):
+        name = Startup.query.filter_by(company_name=company_name.data).first()
+        if name is not None:
+            raise ValidationError("Company name already taken. Try again?")
+
+class FreelancerForm(FlaskForm):
+    first = StringField("First name", validators=[DataRequired()])
+    last = StringField("Last name", validators=[DataRequired()])
+    occupation = SelectField("What job do you do?", choices=jobs_tup, validators=[DataRequired()])
+    submit = SubmitField("Finish Registration for now!")
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,8 @@ from app import app, db
 from app.models import Startup, Job, User
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
-from app.forms import LoginForm, FirstRegistrationForm, SecondRegistrationForm
+from app.forms import LoginForm, FirstRegistrationForm, SecondRegistrationForm,\
+EmployerRegistrationForm, FreelancerForm
 from werkzeug.urls import url_parse
 
 @app.route('/')
@@ -64,10 +65,12 @@ def start_registration():
         This doesn't work.. how do we automatically log in a user after they have logged in?
         """
         return redirect(url_for('second_registration', user=user.username))
-    return render_template('start_registration.html', form=form)
+    return render_template('registration/start_registration.html', form=form)
 
-@app.route('/register2', methods=['GET', 'POST'])
+@app.route('/regiser/2', methods=['GET', 'POST'])
 def second_registration():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     user = User.query.filter_by(username=request.args['user']).first()
     form = SecondRegistrationForm()
     if form.validate_on_submit():
@@ -77,14 +80,56 @@ def second_registration():
             return redirect(url_for('employer_registration', user=user.username))
         elif form.role.data == "Freelancer":
             return redirect(url_for('freelancer_registration', user=user.username))
-    return render_template('second_registration.html', title='Which are you?', form=form)
+    return render_template('registration/second_registration.html', title='Which are you?', form=form)
 
-@app.route('/employer_registration')
+@app.route('/register/employer', methods=['GET', 'POST'])
 def employer_registration():
-    user = User.query.filter_by(username=request.args['user']).first()
-    return "Employer registration {0}, {1}".format(user.username, user.role)
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    """
+    This passing of variables through the url needs to be fixed.
+    Security concerns, maybe using a session variable?
 
-@app.route('/freelancer_registration')
-def freelancer_registration():
+    """
     user = User.query.filter_by(username=request.args['user']).first()
-    return "Freelancer registration {0}, {1}".format(user.username, user.role)
+    form = EmployerRegistrationForm()
+    if form.validate_on_submit():
+        startup_kwargs = {
+        'company_name': form.company_name.data,
+        'state_of_incorporation': form.state_of_incorporation.data,
+        'company_type': form.company_type.data,
+        'taxID': form.taxID.data
+        }
+        startup = Startup(**startup_kwargs)
+        db.session.add(startup)
+        user.create_startup(startup)
+        db.session.commit()
+        login_user(user)
+        flash("Successfully registered {0} with company {1}".format(user.username, startup.company_name))
+        return redirect(url_for('login'))
+    return render_template('registration/employer_registration.html', title='Register your company!', form=form)
+
+@app.route('/register/freelancer', methods=['GET', 'POST'])
+def freelancer_registration():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    user = User.query.filter_by(username=request.args['user']).first()
+    form = FreelancerForm()
+    if form.validate_on_submit():
+        user.first, user.last, user.occupation = form.first.data, form.last.data, form.occupation.data
+        db.session.commit()
+        flash("Successfully created user profile {0}, with first {1} and last {2}".format(user.username, user.first, user.last))
+        return redirect(url_for('login'))
+    return render_template('registration/freelancer_registration.html', title='Register!', form=form)
+
+@app.route('/freelancers/available')
+def freelancers_available():
+    return render_template("dashboards/freelancers_available.html", u=User)
+
+@app.route('/employers/jobs')
+def jobs_available():
+    return render_template("dashboards/jobs_available.html", title='Jobs available', j=Job)
+# @app.route('user/<username>')
+# def user(username):
+#     user = User.query.filter_by(username=username).first_or_404()
+#     return "pass"
